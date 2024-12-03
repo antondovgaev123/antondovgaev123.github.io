ebc92abbd13ad21cf283f48ddefd5ccd0bc62f05
antondovgaev123 <antondovgaev123@gmail.com>
2024-12-03 07:26:55 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url='https://antondovgaev123.github.io/')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
